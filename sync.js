e4be938372ce4c5d608293b85be0606c8e246c19
Protex <petermaggio@sbcglobal.net>
2014-11-30 16:17:49 -0600

    Fixed an error with an unknown function

--- sync.js
+++ sync.js
@@ -18,10 +18,6 @@ var sync = {
 		registered: false,
 		versionComment: 'Initial version'
 	},
-	
-	/*
-	 * 
-	 */
 
 	/*
 	 * Function: register
@@ -40,31 +36,74 @@ var sync = {
     init: function () {return;},
     
     /*
-     * Function: userDataCheck
+     * Function: userDataUpdate
      * 
      * Description: Checks if any of the userData hashes need updated
      * 
-     * Parameters: *dry_run* - bool - set true if encase actual ajax calls are not wanted
+     * Parameters: *bool* - dry_run - set true if encase actual ajax calls are not wanted
      * 
-     * Returns: *array* - Contains all hashes that need/were updated 
+     * Returns: *array* *bool* - Contains all hashes that need/were updated, or false if error occurred 
      * 
      */
     
-    userDataCheck: function (dry_run) {
-    	var hash = vitals.shop.userDataHash,
-    		changedHashes = [];
-    	for ( var i in hash ) {
-    		if ( hash[i].hasBeenChanged == true ) {
-    			changedHashes.push(hash[i]);
-    		} 
+    userDataUpdate: function (dry_run) {
+    	if ( JSON.stringify( vitals.shop.userDataHash ) == "{}" ) {
+	    	var hash = vitals.shop.userDataHash,
+	    		changedHashes = [];
+	    	for ( var i in hash ) {
+	    		if ( hash[i].hasBeenChanged == true ) {
+	    			changedHashes.push(hash[i]);
+	    		} 
+	    	}
+	    	if (dry_run != true) {
+	    		for( var i in changedHashes ) {
+	    			changedHashes[i].update();
+	    		}
+	    		return true;
+	    	} else {
+	    		return changedHashes;
+	    	}
     	}
-    	if (dry_run != true) {
-    		for( var i in changedHashes ) {
-    			changedHashes[i].update();
+    	return false;
+    },
+    
+    /*
+     * Function: arraySync
+     * 
+     * Description: Updates an array of data hashes
+     * 
+     * Parameters:  *array* - data_hashes - the array of data hashes that needs updated
+     * 
+     * Returns: *bool*
+     */
+    
+    arraySync: function(data_hashes) {
+    	if ( (typeof data_hashes).toUppercase() == "ARRAY") {
+    		if ( array[i].hasOwnProperty('update') ) { 
+	    		for (var i in data_hashes) {
+	    			data_hashes[i].update();
+	    		}
+	    		return true;
     		}
-    	} else {
-    		return changedHashes;
     	}
-    }
+    	return false;
+    },
+    
+    /*
+     * Function: syncCheck
+     * 
+     * Description: Checks if a sync is needed
+     * 
+     * Parameters: *none*
+     * 
+     * Returns: *bool*
+     */
+	
+	syncCheck: function () {
+		if ( this.userDataUpdate(true).length != 0 && this.userDataUpdate(true) !== false ) {
+			return true;
+		}
+		return false;
+	}
 	
 };
--- userData.js
+++ userData.js
@@ -74,7 +74,7 @@ var userData = {
             
         var plugin = pb.plugin.get('gold_shop_v4'),
             sKey = proboards.plugin.keys.data['gold_shop_v4'],
-            usersOnPage = Object.keys(proboards.plugin.keys.permissions(['gold_shop_v4'])),          
+            usersOnPage = Object.keys(proboards.plugin.keys.permissions['gold_shop_v4']),          
             self = this;
             
             if ( $.inArray( user.toString(), usersOnPage ) > -1 ) {
